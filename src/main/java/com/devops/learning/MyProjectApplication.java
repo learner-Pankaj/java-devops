c69fc5dff69061e71b4937a0b7e2a2ec80c98394
pankaj <pnkj@email.con>
2025-01-12 16:49:37 +0530

    project build config

---
+++ src/main/java/com/devops/learning/MyProjectApplication.java
@@ -0,0 +1,14 @@
+package com.devops.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(MyProjectApplication.class, args);
+		System.out.println("Hello Pankaj");
+	}
+
+}
